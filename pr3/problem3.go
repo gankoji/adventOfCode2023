c15ea9dc0a869cfec43b3e6a11222d052fbcbafd
Jake Bailey <asaxplayinghorse@gmail.com>
2023-12-02 10:56:55 -0800

    Day 2, problem 3 done

---
+++ pr3/problem3.go
@@ -0,0 +1,106 @@
+/*
+
+You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
+
+The Elf explains that you've arrived at Snow Island and apologizes for the lack of snow. He'll be happy to explain the situation, but it's a bit of a walk, so you have some time. They don't get many visitors up here; would you like to play a game in the meantime?
+
+As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
+
+To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
+
+You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+
+For example, the record of a few games might look like this:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
+
+The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
+
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"strconv"
+)
+
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+	    log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	
+	// This map holds our "total dice put into the bag," by color string
+	var numberOfDice map[string]int
+	numberOfDice = make(map[string]int)
+	
+	numberOfDice["red"]   = 12
+	numberOfDice["green"] = 13
+	numberOfDice["blue"]  = 14
+
+	// The sum of the game IDs we find are valid
+	sum := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// All games are valid until proven otherwise
+		validGame := true
+
+		// Format of line is encoded into the splits
+		// First is getting the game ID ahead of the colon
+		res1 := strings.Split(line, ":")
+		gameNumberStr := strings.Split(res1[0]," ")[1]
+		gameNumber, _ := strconv.Atoi(gameNumberStr)
+
+		// Next, split the game by round (semicolons)
+		res2 := strings.Split(res1[1], ";")
+		for _, round := range res2 {
+			// In each round, we have a number of different color cubes found, separated by comma
+			res3 := strings.Split(round, ",")
+			for _, chunk := range res3 {
+				// Each "color found" is a number and a word, separated by space
+				res4 := strings.Split(strings.TrimSpace(chunk), " ")
+				numCubesSeen, _ := strconv.Atoi(res4[0])
+				colorSeen := strings.TrimSpace(res4[1])
+
+				// Finally, check if that number of (color) is valid...
+				validGame = numCubesSeen <= numberOfDice[colorSeen]
+
+				// ... and escape the loop if it isn't
+				if !validGame {
+					break
+				}
+			}
+
+			// Keep escaping, we don't need to check any other rounds to know the game is bunk
+			if !validGame {
+				break
+			}
+		}
+
+		// Only add the ID if it was a valid game
+		if validGame {
+			sum += gameNumber
+		}
+	}
+
+	// Print the result, of course!
+	fmt.Println("Sum of game IDs: ", sum)
+}
